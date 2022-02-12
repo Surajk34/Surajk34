68db0a4ae8892883989b1fae9d819d3ac2dd352a
Suraj Kurrewar <mesuraj.kurrewar@gmail.com>
2022-02-12 18:55:40 +0530

    Added test case for file upload

--- src/test/java/com/demo/upload/FileControllerTests.java
+++ src/test/java/com/demo/upload/FileControllerTests.java
@@ -1,11 +1,28 @@
 package com.demo.upload;
 
+import com.demo.upload.exception.ServiceException;
+import com.demo.upload.service.impl.FileStorage;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class FileControllerTests {
 
-    //@Test
+    private FileStorage fileStorage = Mockito.mock(FileStorage.class);
 
+    @Test
+    public void testUploadFile() throws ServiceException, IOException {
+        final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("logfile.txt");
+        final MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "logfile.txt", "application/json", inputStream);
+
+        fileStorage.storeFile(mockMultipartFile);
+
+    }
 }
